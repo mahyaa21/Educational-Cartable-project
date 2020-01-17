56fde7796cd20b57467bdfa8e6f52f9e21cd66ca
mahya <mahyaka12@gmail.com>
2020-01-17 11:50:04 +0330

    UPDATE: routes bug

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,21 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
 
-
+import AddNewUser from './_component/admin/AddNewUser';
+import RegisterCourseUser from './_component/admin/registerCourseUser';
+import DownloadStudentHomework from './_component/student/DownloadHomeworks'
+import UploadStudentHomework from './_component/student/UploadHomeworks';
+import StudentHomeworks from './_component/student/Homeworks';
+import DownloadTeacherHomework from './_component/teacher/DownloadHomeworks'
+import UploadTeacherHomework from './_component/teacher/UploadHomeworks';
+import TeacherHomeworks from './_component/teacher/Homeworks';
+import Chat from './_component/chat/Chat';
+import Teacher from './_component/teacher/teacher';
+import student from './_component/student/student';
+import courseRegister from './_component/admin/courseRegister';
+import Admin from './_component/admin/index';
+import Register from './_component/register';
+import Login from './_component/Login';
 class App extends Component {
   render() {
     const {lang} = this.props;
@@ -25,7 +39,21 @@ class App extends Component {
                 <div style={{display:'flex',flexDirection:'row',height:'100%'}}>
                 <Route exact path="/" component={ Home } />
                 <div className="container">
-                  <Routes/>
+                <Route exact path="/addnewuser" component={AddNewUser} />
+                <Route exact path="/register" component={Register} />
+    <Route exact path="/login" component={Login} />
+    <Route path="/dashboard" component={Admin} />
+    <Route path='/course' component={courseRegister} />
+    <Route path='/registercourseuser' component={RegisterCourseUser} />
+    <Route path='/teacher' component={Teacher} />
+    <Route path='/student' component={student} />
+    <Route path='/upload-student' component={UploadStudentHomework} />
+    <Route path='/download-student' component={DownloadStudentHomework} />
+    <Route path='/student-homeworks' component={StudentHomeworks} />
+    <Route path='/upload-teacher' component={UploadTeacherHomework} />
+    <Route path='/download-teacher' component={DownloadTeacherHomework} />
+    <Route path='/homeworks-teacher' component={TeacherHomeworks} />
+    <Route path='/chat' component={Chat} />
                 </div>
                  <Sidebar/> 
                 </div>
